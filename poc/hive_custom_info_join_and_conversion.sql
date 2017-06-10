6dc8a03b0b6f601ca8bef8d503d716b215bca23d
Zhang, Jichao <jichao.zhang@emc.com>
2017-06-10 20:22:54 +0800

    Send queied data into ElasticSearch.

--- poc/hive_custom_info_join_and_conversion.sql
+++ poc/hive_custom_info_join_and_conversion.sql
@@ -42,3 +42,32 @@ FROM bcfmcidi_hive b LEFT JOIN cust_ecif_hive c ON b.cust_isn = c.cust_isn;
 --cust_1  Alice   18910001000     female  1990-12-20      Engineer        Beijing City, Haidian
 --cust_2  Bob     18910001001     male    1980-10-12      Manager Shanghai Citi, Yangpu
 --cust_3  Robert  18910001002     male    1988-10-12      NULL    NULL
+
+-- Send queried data into ElasticSearch
+-- Step 1: Create a EXTERNAL TABLE for elasticsearch
+CREATE EXTERNAL TABLE custom_info_es (
+cust_isn STRING,
+cust_name STRING,
+phone_no STRING,
+sex STRING,
+birthday STRING,
+position STRING,
+address STRING
+)
+STORED BY 'org.elasticsearch.hadoop.hive.EsStorageHandler'
+TBLPROPERTIES('es.resource'='custom/cust_info',
+'es.mapping.names'='cust_isn:custIsn,cust_name:custName,phone_no:phoneNumber,sex:gender,birthday:birthday,position:position,address:address');
+
+-- Step 2: Insert the quiered data into that external table.
+INSERT INTO TABLE custom_info_es 
+SELECT b.cust_isn, b.cust_name, b.phone_no, 
+CASE WHEN b.sex =1 THEN 'male' ELSE 'female' END, 
+concat(substr(cast(b.birthday AS STRING),1,4),'-',substr(cast(b.birthday AS STRING),5,2), '-', substr(cast(b.birthday AS STRING),7,2)), 
+c.post,c.address
+FROM bcfmcidi_hive b LEFT JOIN cust_ecif_hive c ON b.cust_isn = c.cust_isn;
+
+-- Step 3: Then you can query these data using ElasticSearch very easily. And you can leverage all query capbility of ElasticSearch.
+-- curl -XGET 'http://localhost:9200/custom/cust_info'
+-- curl -XGET 'http://localhost:9200/custom/_search?pretty=true'
+-- curl -XGET 'http://localhost:9200/custom/cust_info/_search?q=custIsn:cust_1'
+-- curl -XGET 'http://localhost:9200/custom/cust_info/_search?q=custName:Robert'
