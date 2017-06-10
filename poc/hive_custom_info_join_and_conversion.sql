bb7f7abe0a852329bb65fea51dfedd24362974f8
Zhang, Jichao <jichao.zhang@emc.com>
2017-06-10 19:33:04 +0800

    Add comments

--- poc/hive_custom_info_join_and_conversion.sql
+++ poc/hive_custom_info_join_and_conversion.sql
@@ -1,4 +1,6 @@
 -- Script worked well on Hive 1.2.2
+
+--Prepare table and data.
 DROP TABLE bcfmcidi_hive;
 CREATE TABLE bcfmcidi_hive (
 cust_isn STRING,
@@ -15,8 +17,6 @@ INSERT INTO TABLE bcfmcidi_hive VALUES
 ('cust_3', 'org_3', '18910001002', 'Robert', 1, 19881012.0)
 ;
 
-SELECT * FROM bcfmcidi_hive;
-
 DROP TABLE cust_ecif_hive;
 CREATE TABLE cust_ecif_hive (
 cust_isn STRING,
@@ -31,8 +31,14 @@ INSERT INTO cust_ecif_hive VALUES
 ('cust_2', 'org_2', 'Manager', '020-99202203', 'Shanghai Citi, Yangpu');
 
 
+-- Query based on requirements
 SELECT b.cust_isn, b.cust_name, b.phone_no, 
 CASE WHEN b.sex =1 THEN 'male' ELSE 'female' END, 
 concat(substr(cast(b.birthday AS STRING),1,4),'-',substr(cast(b.birthday AS STRING),5,2), '-', substr(cast(b.birthday AS STRING),7,2)), 
 c.post,c.address
 FROM bcfmcidi_hive b LEFT JOIN cust_ecif_hive c ON b.cust_isn = c.cust_isn;
+
+--The ouput will be like below:
+--cust_1  Alice   18910001000     female  1990-12-20      Engineer        Beijing City, Haidian
+--cust_2  Bob     18910001001     male    1980-10-12      Manager Shanghai Citi, Yangpu
+--cust_3  Robert  18910001002     male    1988-10-12      NULL    NULL
