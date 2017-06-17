7edda4d487caf22cbe4d796a6065e48966aeeee6
Zhang, Jichao <jichao.zhang@emc.com>
2017-06-17 19:10:45 +0800

    How to access web ui of namenode or hdfs

--- scripts/start-hdfs.sh
+++ scripts/start-hdfs.sh
@@ -12,7 +12,18 @@ $HADOOP_HOME/bin/hdfs datanode #Start the datanode.
 $HADOOP_HOME/sbin/start-dfs.sh
 #--------- Start HDFS use only one script --------#
 
-#--------- Check if HAFS can work -----------#
+#--------- Check if HDFS can work -----------#
 ./hdfs dfs -mkdir /hello_hdfs
 ./hdfs dfs -ls /
-#--------- Check if HAFS can work -----------#
+#--------- Check if HDFS can work -----------#
+
+# WEB UI
+# Step 1: Find the IP Port
+netstat -tuanp | grep <NameNodeProcess>
+tcp        0      0 10.141.49.90:8020       0.0.0.0:*               LISTEN      18240/java
+tcp        0      0 0.0.0.0:50070           0.0.0.0:*               LISTEN      18240/java
+tcp        0      0 10.141.49.90:8020       10.141.68.199:44699     ESTABLISHED 18240/java
+tcp        0      0 10.141.49.90:8020       10.32.253.40:56874      ESTABLISHED 18240/java
+tcp        0      0 10.141.49.90:8020       10.141.68.198:34325     ESTABLISHED 18240/java
+
+# Step 2: Open Chrome and access http://<NameNodeIp>:50070. 
